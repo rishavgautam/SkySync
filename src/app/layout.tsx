ab16f610e2ba15bba885870c348db7d4b23666be
Rishav Gautam <rishav210@gmail.com>
2023-11-23 20:42:34 -0500

    Scrollbar test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import '../styles/scrollbar.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/styles.css'
-
 
 const inter = Inter({ subsets: ['latin'] })
 
